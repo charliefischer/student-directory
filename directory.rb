17b7bb1f8abb4da2aca2b73605f70be374cb01d4
Charlie Fischer <charlie.a.fischer@gmail.com>
2020-12-16 14:21:43 +0000

    .gitignore

--- directory.rb
+++ directory.rb
@@ -43,6 +43,7 @@ def print_menu
 #1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -58,6 +59,18 @@ def show_students
   print_footer
 end
 
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def interactive_menu
   loop do
     print_menu
@@ -69,6 +82,8 @@ def interactive_menu
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       puts "Goodbye"
       exit #this will terminate the program
@@ -77,6 +92,11 @@ def interactive_menu
     end
   end
 end
+
+
+
+
+
 #nothing happens until we call the methods
 interactive_menu
 
