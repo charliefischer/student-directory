8814f7a54f685963eb26a64095456ecef4b6046f
Charlie Fischer <charlie.a.fischer@gmail.com>
2020-12-16 10:42:50 +0000

    amended broken code

--- directory.rb
+++ directory.rb
@@ -14,23 +14,25 @@
 # ]
 def input_students
   puts "To finish, hit enter with no name present"
-  puts "If you make a typo, enter 'typo'"
-  #create an empty array
   students = []
   loop do
     puts "Please enter the name of the student"
-    name = gets.chomp.to_sym
+    name = gets.chop.to_sym.capitalize
     if name.empty?
       break
     end
     puts "Please enter their cohort start date"
-    cohort = gets.chomp.to_sym
+    cohort = gets.chop.to_sym.capitalize
     if cohort.empty?
       cohort = "N/A"
     end
-
-    students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} students"
+    puts "Is this data correct?"
+    puts "(name: #{name}) (cohort: #{cohort})"
+    yes_or_no = gets.chop
+    if yes_or_no.downcase == "yes"
+      students << {name: name, cohort: cohort}
+      puts "Now we have #{students.count} students"
+    end
   end
   students
 end
@@ -74,16 +76,32 @@ end
 #   end
 # end
 
+# def printer(names)
+#   i = 0
+#   until i == names.length
+#     puts ("#{names[i][:name]} (#{names[i][:cohort]} cohort)").center(40)
+#     i += 1
+#   end
+# end
+
 def printer(names)
-  i = 0
-  until i == names.length
-    puts ("#{names[i][:name]} (#{names[i][:cohort]} cohort)").center(40)
-    i += 1
+  puts "Which cohort would you like to view?"
+  month = gets.chomp.to_sym.capitalize
+  puts "Students from the #{month} cohort".center(40)
+  puts "-----------".center(40)
+  names.map { |student|
+    if student[:cohort] == month
+    puts "#{student[:name]}".center(40)
   end
+}
 end
 
 def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+  if names.count < 2
+    puts "Overall we have 1 student"
+  elsif names.count > 1
+    puts "Overall, we have #{names.count} great students".center(40)
+  end
 end
 #nothing happens until we call the methods
 students = input_students
